da022b7c67614eaaeb47b079ea0edf33793ffa2f
Dimitrije Gerencic <dimitrijegerencic@gmail.com>
2022-12-21 22:37:15 +0100

    feat: add dynamic height and width to input and password components

--- src/components/formFields/inputField/InputField.jsx
+++ src/components/formFields/inputField/InputField.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import classes from "./InputField.module.scss";
 import {Input} from "antd";
 
-const InputField = ({label}) => {
+const InputField = ({label, use}) => {
+
+    const h = use === 'profile' ? 40 : 52;
+    const w = use === 'profile' ? 318 : 450;
+
     return <Input placeholder={label}
+                  style={{height: h, width : w}}
                   className={classes["__input_field"]} />;
 }
 
--- src/components/formFields/passwordField/PasswordField.jsx
+++ src/components/formFields/passwordField/PasswordField.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import classes from './PasswordField.module.scss';
 import { Input } from 'antd';
 
-const PasswordField = ({label}) => {
+const PasswordField = ({label, use}) => {
+
+    const h = use === 'profile' ? 40 : 52;
+    const w = use === 'profile' ? 318 : 450;
+
     return <Input.Password
         placeholder={label}
+        style={{height: h, width : w}}
         className={classes["__input_field"]} />
 }
 
