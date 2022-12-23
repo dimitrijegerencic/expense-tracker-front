49d2c62ed21bddcb8f868b092ee3d2c534e85b0f
Dimitrije Gerencic <dimitrijegerencic@gmail.com>
2022-12-23 21:49:59 +0100

    perf: add dynamic color of title for InfoCard component

--- src/components/cards/infoCard/InfoCard.jsx
+++ src/components/cards/infoCard/InfoCard.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import classes from "./InfoCard.module.scss";
 
 const InfoCard = ({title, value, color}) => {
+
+    const c = color.toString();
+
     return <div className={classes['info-card']}>
         <p className={classes['card-title']}>{title}</p>
-        <h1 className={classes['card-value']} style={{color:{this:color}}}>{value}</h1>
+        <h1 className={classes['card-value']} style={{color:c}}>{value}</h1>
     </div>
 }
 export default InfoCard;
